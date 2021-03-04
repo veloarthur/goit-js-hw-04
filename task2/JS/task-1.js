e88953bf055cb1818a4bb719b0a936576989a1fc
Arthur <vasyavasilyev2000@gmail.com>
2021-03-04 22:21:21 +0200

    second commit, edited task2 modul4

--- task2/JS/task-1.js
+++ task2/JS/task-1.js
@@ -3,28 +3,23 @@ const inventory = {
   add(itemName) {
     console.log(`Adding ${itemName} to inventory`);
 
-    inventory.items.push(itemName);
+    this.items.push(itemName);
   },
   remove(itemName) {
     console.log(`Removing ${itemName} from inventory`);
 
-    inventory.items = inventory.items.filter((item) => item !== itemName);
+    this.items = this.items.filter((item) => item !== itemName);
   },
 };
-
 const invokeInventoryAction = function (itemName, action) {
   console.log(`Invoking action on ${itemName}`);
   action(itemName);
 };
 
-invokeInventoryAction("Medkit", inventory.add);
-// Invoking action on Medkit
-// Adding Medkit to inventory
+invokeInventoryAction("Medkit", inventory.add.bind(inventory));
 
-console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+console.log(inventory.items);
 
-invokeInventoryAction("Gas mask", inventory.remove);
-// Invoking action on Gas mask
-// Removing Gas mask from inventory
+invokeInventoryAction("Gas mask", inventory.remove.bind(inventory));
 
-console.log(inventory.items); // ['Knife', 'Medkit']
+console.log(inventory.items);
